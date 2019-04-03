81e77786db34939a41ae17f03db70825f1a2cd97
Onl-Test <tracnghiem.online1990@gmail.com>
2019-04-04 02:43:08 +0700

    ca

--- js/index.js
+++ js/index.js
@@ -1,22 +1,23 @@
 function OpenTestForm() {
+	document.getElementById('button-open-form').style.display = 'none';
 	document.getElementById('test-form').style.display = 'inline-block';
-	document.getElementById('exit-form').style.display = 'inline-block';
 	
 	document.getElementById('countdown').style.display = 'inline-block';
-	var TimeEnd = 900000;
-var TimeStart = 0;
+	var TimeEnd = 5000;
+	var TimeStart = 0;
 
-var x = setInterval(function() {
-	TimeStart += 1000;	
-	var distance = TimeEnd - TimeStart;
-	var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-  	var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+	var x = setInterval(function() {
+		TimeStart += 1000;	
+		var distance = TimeEnd - TimeStart;
+		var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+	  	var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
-  	document.getElementById("clock").innerHTML = minutes + "p " + seconds + "'";
+	  	document.getElementById("clock").innerHTML = minutes + " phút " + seconds + " giây";
 
-	if (distance < 0) {
-	    clearInterval(x);
-	    document.getElementById("countdown").innerHTML = "HẾT GIỜ";
-	  }
-}, 1000);
+		if (distance < 0) {
+		    clearInterval(x);
+		    alert("Bài làm của bạn đã bị hủy!");
+		    document.getElementById('countdown').style.display = 'none';
+		  }
+	}, 1000);
 }
