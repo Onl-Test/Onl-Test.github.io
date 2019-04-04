af80ad8f8332193f31802b46f9b049f5f86bb01b
Onl-Test <tracnghiem.online1990@gmail.com>
2019-04-04 10:04:07 +0700

    Updated 10:04

--- js/index.js
+++ js/index.js
@@ -3,7 +3,10 @@ function OpenTestForm() {
 	document.getElementById('test-form').style.display = 'inline-block';
 	
 	document.getElementById('countdown').style.display = 'inline-block';
+	
+	/* !!! SET UP TIMER HERE !!! */
 	var TimeEnd = 60000;
+	
 	var TimeStart = 0;
 
 	var x = setInterval(function() {
@@ -14,9 +17,15 @@ function OpenTestForm() {
 
 	  	document.getElementById("clock").innerHTML = minutes + " phút " + seconds + " giây";
 
+	  	if (distance < 30000 && distance > 28000) {
+	  		document.getElementById('reminder-tab').style.display = 'inline-block';
+	  	}
+
 		if (distance < 0) {
 		    clearInterval(x);
 		    document.getElementById('cancel-tab').style.display = 'inline-block';
+		    document.getElementById('countdown').style.display = 'none';
+		    document.getElementById('test-form').style.display = 'none';
 		  }
 	}, 1000);
 }
